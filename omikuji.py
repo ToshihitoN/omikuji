045f377a9c2eb1e1f564cbe6be5ca30817e2297f
ToshihitoN <toshimaru.0kara@gmail.com>
2022-06-19 12:13:32 +0900

    first commit

---
+++ omikuji.py
@@ -0,0 +1,10 @@
+# - [ ] 結果リストからランダムに1つの値を出力する
+#     - [ ] 結果リストをまとめて定義する
+#     - [ ] 複数要素からランダムに1つの値を取得する
+#     - [ ] 取得した値を出力する
+
+from random import randint
+import = random
+unsei = (5)
+
+print(unsei)
